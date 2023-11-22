bf6e9d692457d2c5b678be0e3c2dbb1583e614a4
k-cicek <kevserrcicek@gmail.com>
2023-11-22 23:49:31 +0300

    fixed code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useEffect, useRef, useState } from "react";
 import "./App.css";
+import { FaEraser, FaTimes } from "react-icons/fa";
 
 function App() {
   //canvasRef çizim yapılacak olan canvas elementine referans oluyor.
@@ -10,19 +11,28 @@ function App() {
   const [context, setContext] = useState(null);
   const [brushSize, setBrushSize] = useState(2);
   const [brushColor, setBrushColor] = useState("#000000");
+  const [isEraser, setEraser] = useState(false);
 
   //canvas elementine erişilir ve context state'ine atanır.
   useEffect(() => {
     const canvas = canvasRef.current;
     const ctx = canvas.getContext("2d");
     setContext(ctx);
-  });
+
+    //canvas elementinin arkaplan rengi ayarlanır.
+    ctx.fillStyle = "white";
+    //canvas elementinin boyutları ayarlanır.
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+  }, []);
 
   //Fare tuşuna basıldığında çizime başlanır.
   const startDrawing = (e) => {
     const { offsetX, offsetY } = e.nativeEvent;
     context.beginPath();
     context.moveTo(offsetX, offsetY);
+    context.globalCompositeOperation = isEraser
+      ? "destination-out"
+      : "source-over";
     setIsDrawing(true);
   };
 
@@ -34,8 +44,14 @@ function App() {
     //kalem boyutu ayarlanır.
     context.lineWidth = brushSize;
 
-    //kalem rengi ayarlanır.
-    context.strokeStyle = brushColor;
+    //Eraser aktif ise kalem rengi beyaz yapılır.
+    if (isEraser) {
+      context.globalCompositeOperation = "destination-out";
+      context.strokeStyle = "white";
+    } else {
+      context.globalCompositeOperation = "source-over";
+      context.strokeStyle = brushColor;
+    }
 
     context.lineTo(offsetX, offsetY);
     context.stroke();
@@ -45,6 +61,26 @@ function App() {
   const finishDrawing = () => {
     context.closePath();
     setIsDrawing(false);
+  };
+
+  //Eraser aktif ise kalem rengi beyaz yapılır.
+  const toggleEraser = () => {
+    setEraser(!isEraser);
+  };
+
+  const clearCanvas = () => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext("2d");
+    ctx.globalCompositeOperation = isEraser ? "destination-out" : "source-over";
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    ctx.globalCompositeOperation = "source-over";
+
+    setEraser(false);
+
+    ctx.fillStyle = "white";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
   };
 
   return (
@@ -78,6 +114,12 @@ function App() {
         value={brushColor}
         onChange={(e) => setBrushColor(e.target.value)}
       />
+      <button onClick={toggleEraser}>
+        <FaEraser />
+      </button>
+      <button onClick={clearCanvas}>
+        <FaTimes />
+      </button>
     </div>
   );
 }
