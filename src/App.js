cedc71030f5e06fccfcad6ea1140ad9874a937ab
k-cicek <kevserrcicek@gmail.com>
2023-11-23 15:25:31 +0300

    undo method added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useEffect, useRef, useState } from "react";
 import "./App.css";
-import { FaEraser, FaTimes } from "react-icons/fa";
+import { FaEraser, FaTimes, FaUndo } from "react-icons/fa";
 
 function App() {
   //canvasRef çizim yapılacak olan canvas elementine referans oluyor.
@@ -12,6 +12,7 @@ function App() {
   const [brushSize, setBrushSize] = useState(2);
   const [brushColor, setBrushColor] = useState("#000000");
   const [isEraser, setEraser] = useState(false);
+  const [drawHistory, setDrawHistory] = useState([]);
 
   //canvas elementine erişilir ve context state'ine atanır.
   useEffect(() => {
@@ -30,31 +31,34 @@ function App() {
     const { offsetX, offsetY } = e.nativeEvent;
     context.beginPath();
     context.moveTo(offsetX, offsetY);
-    context.globalCompositeOperation = isEraser
-      ? "destination-out"
-      : "source-over";
+
+    // Eraser modu ise rengi beyaz olarak ayarla
+    context.strokeStyle = isEraser ? "white" : brushColor;
     setIsDrawing(true);
+
+    // Çizim geçmişini temizle
+    setDrawHistory([]);
   };
 
   //Fare hareket ettirildiğinde çizim devam eder.
   const draw = (e) => {
-    if (!isDrawing) return;
+    if (!isDrawing || !context) return;
     const { offsetX, offsetY } = e.nativeEvent;
 
-    //kalem boyutu ayarlanır.
-    context.lineWidth = brushSize;
+    context.lineWidth = brushSize; //kalem boyutu ayarlanır.
+    context.lineCap = "round"; // Çizgi uçları yuvarlak olsun
 
-    //Eraser aktif ise kalem rengi beyaz yapılır.
-    if (isEraser) {
-      context.globalCompositeOperation = "destination-out";
-      context.strokeStyle = "white";
-    } else {
-      context.globalCompositeOperation = "source-over";
-      context.strokeStyle = brushColor;
-    }
+    // Eraser modu ise rengi beyaz olarak ayarla
+    context.strokeStyle = isEraser ? "white" : brushColor;
 
     context.lineTo(offsetX, offsetY);
     context.stroke();
+
+    //çizim adımını kaydet
+    setDrawHistory((prev) => [
+      ...prev,
+      context.getImageData(0, 0, context.canvas.width, context.canvas.height),
+    ]);
   };
 
   //Fare tuşu bırakıldığında veya canvas elementinden çıkıldığında çizim biter.
@@ -71,20 +75,40 @@ function App() {
   const clearCanvas = () => {
     const canvas = canvasRef.current;
     const ctx = canvas.getContext("2d");
-    ctx.globalCompositeOperation = isEraser ? "destination-out" : "source-over";
 
+    // Eraser modu açıkken, kalem rengini beyaz yap ve modu kapat
+    if (isEraser) {
+      ctx.strokeStyle = "white";
+      setEraser(false); // Eraser modunu kapat
+    }
+
+    // Canvas'ı temizle
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    ctx.globalCompositeOperation = "source-over";
-
-    setEraser(false);
-
+    // Temizlenmiş canvas'ı beyaz renkle doldur
     ctx.fillStyle = "white";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    //Canvas temizlendiğinde çizim adımlarını temizler
+    setDrawHistory([]);
+  };
+
+  const undoDraw = () => {
+    if (drawHistory.length > 1) {
+      //son çizim adımını sil
+      drawHistory.pop();
+      //canvas'ı temizle
+      context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+      //çizim adımlarını geri al
+      context.putImageData(drawHistory[drawHistory.length - 1], 0, 0);
+    } else {
+      //Geri alınacak çizim adımı kalmadıysa canvas'ı temizle
+      clearCanvas();
+    }
   };
 
   return (
-    <div>
+    <div className="">
       <canvas
         className="cursor-crosshair	border border-solid border-black"
         width={800}
@@ -95,31 +119,40 @@ function App() {
         onMouseUp={finishDrawing}
         onMouseOut={finishDrawing}
       ></canvas>
-      <label htmlFor="brushSize">Pen Size:</label>
-      <input
-        type="range"
-        id="brushSize"
-        name="brushSize"
-        min="1"
-        max="20"
-        value={brushSize}
-        onChange={(e) => setBrushSize(parseInt(e.target.value))}
-      />
+      <div className="flex flex-row justify-between">
+        <div>
+          <label htmlFor="brushSize">Pen Size:</label>
+          <input
+            type="range"
+            id="brushSize"
+            name="brushSize"
+            min="1"
+            max="20"
+            value={brushSize}
+            onChange={(e) => setBrushSize(parseInt(e.target.value))}
+          />
+        </div>
+        <div>
+          <label htmlFor="brushColor">Pen Color:</label>
+          <input
+            type="color"
+            id="brushColor"
+            name="brushColor"
+            value={brushColor}
+            onChange={(e) => setBrushColor(e.target.value)}
+          />
+        </div>
 
-      <label htmlFor="brushColor">Pen Color:</label>
-      <input
-        type="color"
-        id="brushColor"
-        name="brushColor"
-        value={brushColor}
-        onChange={(e) => setBrushColor(e.target.value)}
-      />
-      <button onClick={toggleEraser}>
-        <FaEraser />
-      </button>
-      <button onClick={clearCanvas}>
-        <FaTimes />
-      </button>
+        <button onClick={toggleEraser}>
+          <FaEraser />
+        </button>
+        <button onClick={clearCanvas}>
+          <FaTimes />
+        </button>
+        <button onClick={undoDraw}>
+          <FaUndo />
+        </button>
+      </div>
     </div>
   );
 }
